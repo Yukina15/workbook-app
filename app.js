18f0212653eda1eefc0da45f45f5fae8c09955d6
YUKINA <bluesky.nightsky.starrysky@gmail.com>
2025-08-29 19:14:02 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function addWord() {
+    const eng = document.getElementById(`english`).value.trim();
+    const jpn = document.getElementById(`japanese`).value.trim();
+    if (eng === `` || jpn === ``)return;
+
+    const li = document.createElement(`li`);
+    li.textContent = `${eng} → ${jpn}`;
+    document.getElementById(`wordList`).appendChild(li);
+
+    document.getElementById(`english`).value = ``;
+    document.getElementById(`japanese`).value = ``;
+}
