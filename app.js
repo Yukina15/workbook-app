53379501b8f2782776b8242a7e3cbd2d206bcd99
YUKINA <bluesky.nightsky.starrysky@gmail.com>
2025-10-10 19:50:31 +0900

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+let flashcards = [];
+
+function saveWords() {
+    localStorage.setItem('flashcards', JSON.stringify(flashcards));
+}
+
 
 function addWord() {
     const eng = document.getElementById(`english`).value.trim();
     const jpn = document.getElementById(`japanese`).value.trim();
-    if (eng === `` || jpn === ``)return;
 
-    const li = document.createElement(`li`);
-    li.textContent = `${eng} → ${jpn}`;
-    document.getElementById(`wordList`).appendChild(li);
+    if (eng === `` || jpn === ``){
+        alert('英単語と日本語訳を入力してください。');
+        return;
+    }
 
-    document.getElementById(`english`).value = ``;
-    document.getElementById(`japanese`).value = ``;
+    const newCard = {
+        english: eng,
+        japanese: jpn
+    };
+
+    flashcards.push(newCard)
+    saveWords();
+
+    document.getElementById('result').innerText = '登録完了';
+
+    // const li = document.createElement(`li`);
+    // li.textContent = `${eng} → ${jpn}`;
+    // document.getElementById(`wordList`).appendChild(li);
+
+    // document.getElementById(`english`).value = ``;
+    // document.getElementById(`japanese`).value = ``;
 }
