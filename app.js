50e9ef6f8cae2173d6afe09b39043e8f82ef64fe
YUKINA <bluesky.nightsky.starrysky@gmail.com>
2025-11-28 19:07:56 +0900

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+
 let flashcards = [];
 
-function saveWords() {
-    localStorage.setItem('flashcards', JSON.stringify(flashcards));
-}
+let wordnum;
+
 
 
 function addWord() {
+
+
+
     const eng = document.getElementById(`english`).value.trim();
     const jpn = document.getElementById(`japanese`).value.trim();
 
@@ -19,15 +22,44 @@ function addWord() {
         japanese: jpn
     };
 
-    flashcards.push(newCard)
+
+    wordnum = wordnum + 1;
+
+    flashcards.push(newCard);
     saveWords();
 
-    document.getElementById('result').innerText = '登録完了';
+    // flashcards = JSON.parse(localStorage.getItem('flashcards'));
+
+    document.getElementById(`english`).value = ``;
+    document.getElementById(`japanese`).value = ``;
+    document.getElementById(`result`).innerText = '登録完了';
+
+
+    document.getElementById('wordnum').innerText = '登録単語数　：　' + wordnum;
+
 
     // const li = document.createElement(`li`);
     // li.textContent = `${eng} → ${jpn}`;
-    // document.getElementById(`wordList`).appendChild(li);
 
-    // document.getElementById(`english`).value = ``;
-    // document.getElementById(`japanese`).value = ``;
+}
+
+function saveWords() {
+    localStorage.setItem('flashcards', JSON.stringify(flashcards));
+    localStorage.setItem('wordnum',JSON.stringify(wordnum));
+}
+
+window.onload = function() {
+    flashcards = JSON.parse(localStorage.getItem('flashcards'));
+    wordnum = JSON.parse(localStorage.getItem('wordnum'));
+
+    document.getElementById('wordnum').innerText = '登録単語数　：　' + wordnum;
+};
+
+
+
+function cleardeta() {
+
+    localStorage.setItem('flashcards',JSON.stringify([]));
+    wordnum = 1;
+
 }
